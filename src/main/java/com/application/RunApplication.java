ecacfb203e85f738dc27f2f57d8780993f7717d6
JermaineBeea <jebeeajhb024@student.wethinkcode.co.za>
2025-08-05 09:00:08 +0200

    commit

---
+++ src/main/java/com/application/RunApplication.java
@@ -0,0 +1,44 @@
+package com.application;
+import java.util.*;;
+
+public class RunApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Calculator Application!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        double num1 = scanner.nextDouble();
+        System.out.print("Enter second number: ");
+        double num2 = scanner.nextDouble();
+        System.out.print("Enter an operator (+, -, *, /): ");
+        char operator = scanner.next().charAt(0);
+        double result;
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                result = num1 / num2;
+                break;
+            default:
+                System.out.println("Invalid operator!");
+                scanner.close();
+                System.out.println("Please restart the application and try again.");
+                return;
+        }
+        System.out.println("Result: " + result);
+
+        scanner.close();
+        System.out.println("Thank you for using the Calculator Application!");
+    }
+
+        
+}
+    
+
